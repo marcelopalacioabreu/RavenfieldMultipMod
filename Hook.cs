5e6be0715b7c0ae8dc9b3d7e83791f4ae424255a
Sam Byass <sam@whcuk.net>
2017-08-01 10:32:01 +0100

    Cleanup, fix build, change .NET dependency to be the same one the game uses (3.5), and add Hook.cs to project.

--- Hook.cs
+++ Hook.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RFMultipMod
 {
     public class Hook
     {
-        public static void startMultiplayerMod()
+        public static void StartMultiplayerMod()
         {
             Console.WriteLine("Ravenfield Multiplayer Mod is GO for launch."); //This will write to output.log.
+            //TODO: Get some runtime level hooks injected here, see https://www.codeproject.com/articles/463508/net-clr-injection-modify-il-code-on-run-time
         }
     }
 }
