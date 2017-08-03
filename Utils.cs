63986ecb0fc2bc1758461a440eb33567a29d2b5f
Miles <milesfm100@gmail.com>
2017-08-03 21:07:48 +1000

    Added All Scene Log

--- Utils.cs
+++ Utils.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using UnityEngine;
 
 namespace RFMultipMod
 {
-    public class Utils
+    public class Utils : MonoBehaviour
     {
         public static void Log(String message)
         {
             Console.WriteLine("[INFO] [RFMpMod] " + message);
+        }
+        /// <summary>
+        /// Logs all the gameobjects in a scene
+        /// </summary>
+        public static void LogAllGameObjects()
+        {
+            Log("All GameObjects in Scene: ");
+            GameObject[] allGO = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+            foreach (var oneGO in allGO)
+            {
+                Log(oneGO.name);
+            }
         }
     }
 }
