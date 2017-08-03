b601c6f8819a6558a4a732d4eb31fae772bb471b
Miles <milesfm100@gmail.com>
2017-08-03 18:24:32 +1000

    Changed Script and HUD

--- NetworkScriptandHUD.cs
+++ NetworkScriptandHUD.cs
@@ -1,6 +1,23 @@
-﻿namespace RFMultipMod
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine.Networking.Match;
+
+namespace RFMultipMod
 {
-    class NetworkScriptandHUD
+    class NetworkScriptandHUD : MonoBehaviour
     {
+        NetworkManagerHUD HUDManager;
+        NetworkManager ManagerNet;
+        NetworkScriptandHUD()
+        {
+            HUDManager = gameObject.AddComponent<NetworkManagerHUD>();
+            ManagerNet = gameObject.AddComponent<NetworkManager>();
+        }
     }
+
+    // Code for NetworkManagerHUD: https://pastebin.com/EbVQNpsS
+    // Code for NetworkManager: https://pastebin.com/HzdPGR3N
 }
